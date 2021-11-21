0fec9a396e439a6bc3c0a8d73ac00c06f5e700ed
Ekaterina <kitekate2000cat@gmail.com>
2021-11-21 13:55:17 +0300

    some minor additions to resource and interaction classes

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections.Generic;
 public class ResourceManager
 {
-    private Dictionary<ResourceType, int> resorces;
+    private Dictionary<ResourceType, int> resources;
     public void AddResource(ResourceType resource, int quantity)
     {
-        
+        //todo
     }
 
 }
