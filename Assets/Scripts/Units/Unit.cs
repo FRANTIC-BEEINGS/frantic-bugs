63883aeac550f9a65ffbd0d33d10c51ffd50916b
Sofiika <sofi.k.andreeva@mail.ru>
2021-11-21 21:24:46 +0300

    add units movement

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -15,8 +15,17 @@ public class Unit : MonoBehaviour
     [SerializeField] double increaseCoef;
     [SerializeField] double decreaseCoef;
     [SerializeField] int sight;
+    [SerializeField] private Sprite sprite;
+    private Vector3 endPosition;
+    [SerializeField] private float movingTime = 1f;
+    private bool isStopMovement = false;
 
-    // ��������� ������, ���������� ���� � ���������� �������
+    public void stopMovement()
+    {
+        isStopMovement = true;
+    }
+
+    // increase level and change characteristics 
     public void IncreaseLevel()
     {
         level += 1;
@@ -27,53 +36,83 @@ public class Unit : MonoBehaviour
         resourceEnergy = (int)(resourceEnergy * decreaseCoef);
     }
 
-    // �������� � �����.
-    // mobForce - ���� ����
-    // allEnergy - ���������� ������� ������
-    // levelupMob - ����� �� �������� ���� �������� ������� �����
-    public void FightMob(int mobForce, ref int allEnergy, bool levelupMob) //Enemy card
+    //for movement test
+    private void OnGUI()
     {
-        if (mobForce < force)
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (GUILayout.Button("Stop moving")) stopMovement();
+        if (GUILayout.Button("Start again")) Start();
+        GUILayout.EndArea();
+    }
+    
+
+    IEnumerator Move(List<Card> cards, int energy)
+    {
+        foreach (Card card in cards)
         {
-            allEnergy -= fightEnergy;
-            if (allEnergy < 0)
+            if (isStopMovement)
             {
-                Death();
-                return;
+                isStopMovement = false;
+                break;
             }
-            //todo �������� �����, ����� ������ �������
 
-            if (levelupMob)
-                IncreaseLevel();
+            endPosition = card.gameObject.transform.position; // find destination position
+            if (energy < moveEnergy)
+                break;
+            energy -= moveEnergy;
+            yield return StartCoroutine(MoveTo(endPosition, movingTime)); //start one movement
         }
-        else
+    }
+    
+    // for movement test
+    IEnumerator MoveTest(List<Vector3> cards, int energy)
+    {
+        foreach (Vector3 card in cards)
         {
-            Death();
+            if (isStopMovement)
+            {
+                isStopMovement = false;
+                break;
+            }
+            
+            endPosition = card;
+            if (energy < moveEnergy)
+                break;
+            energy -= moveEnergy;
+            yield return StartCoroutine(MoveTo(endPosition, movingTime));
         }
     }
-
-    // ������ ����� �����
-    public void CaptureCard(ref int allEnergy)
+    
+    
+    IEnumerator MoveTo(Vector3 position, float time)
     {
-        if (captureEnergy > allEnergy)
-            return;
-
-        allEnergy -= captureEnergy;
-        // todo �������� �����, ��� ��� ���������
+        Vector3 start = transform.position;
+        Vector3 end = position;
+        float t = 0;
+        
+        //in every moment move to destination
+        while(t < 1)
+        {
+            yield return null;
+            t += Time.deltaTime / time;
+            transform.position = Vector3.Lerp(start, end, t);
+        }
+        transform.position = end;  
     }
 
-    public void GetResource(ref int allEnergy)
+    private void Start()
     {
-        if (resourceEnergy > allEnergy)
-            return;
-
-        allEnergy -= resourceEnergy;
-        // todo �������� ��� ������� � �������� ������ � ����� �������
-        // todo �������� �����, ����� ������ ������
+        //test movement
+        List<Vector3> cards = new List<Vector3>()
+        {
+            new Vector3(5, -2, 0), new Vector3(4, 1, 0), new Vector3(-3, 2, 0), 
+            new Vector3(0, 0, 0), new Vector3(5, -2, 0)
+        };
+        StartCoroutine(MoveTest(cards, 100));
     }
 
     private void Death() 
     {
-        //todo
+        Destroy(this);
     }
 }
