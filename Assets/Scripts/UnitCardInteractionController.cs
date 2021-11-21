70685ae65f0ae95c747dd0181d39b01ef681dea9
Ekaterina <kitekate2000cat@gmail.com>
2021-11-21 13:38:40 +0300

    add some default functions in interaction controller

--- Assets/Scripts/UnitCardInteractionController.cs
+++ Assets/Scripts/UnitCardInteractionController.cs
@@ -3,4 +3,16 @@
 public class UnitCardInteractionController
 {
     private ResourceManager _resourceManager;
+    public void FightEnemy(EnemyCard enemyCard)
+    {
+        
+    }
+
+    public void CaptureCard(Card card)  //поменять на тип интерфейса позже (или что-то такое надо подумать)
+    {
+    }
+
+    public void GetResource(ResourceCard resourceCard)
+    {
+    }
 }
