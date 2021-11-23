972021fc10c5c3ab265ece8c4cd4c7e90d3868e3
Ekaterina <kitekate2000cat@gmail.com>
2021-11-23 20:52:26 +0300

    add replenishable resource management functions

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -1,10 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 public class ResourceManager
 {
     private Dictionary<ResourceType, int> resources;
-    public void AddResource(ResourceType resource, int quantity)
+    public Action<ResourceCard> GlobalReplenish;
+
+    public ResourceManager()
     {
-        //todo
+        resources = new Dictionary<ResourceType, int>();
     }
 
+    public void AddResource(ResourceType resource, int quantity)
+    {
+        if (resources.ContainsKey(resource))
+        {
+            resources[resource] += quantity;
+        }
+        else
+        {
+            resources.Add(resource,quantity);
+        }
+    }
+
+    //adds replenishment func (must be called when captured resource card)
+    public void AddReplenishableResource()
+    {
+        //todo: write test function
+        GlobalReplenish += Replenish;
+    }
+
+    public void RemoveReplenishableResource()
+    {
+        GlobalReplenish -= Replenish;
+    }
+
+    private void Replenish(ResourceCard resourceCard)
+    {
+        bool replenishNow = resourceCard.ReplenishTick();
+        if (!replenishNow)
+            return;
+        AddResource(resourceCard.GetResource(),resourceCard.GetReplenishResourceCount());
+    }
 }
