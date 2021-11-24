3d236488628d94135f5d0b00efeea6cab8e89451
ZakharovI <trolev_2012@mail.ru>
2021-11-24 20:15:42 +0300

    removed a lot of useless code

--- Assets/Scripts/PathBuilder.cs
+++ Assets/Scripts/PathBuilder.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 enum MouseButtons {
     Left = 0,
     Right = 1,
-    Nothing = 2,
-    Both = 3
+    Nothing,
+    Both
 }
 
 public class PathBuilder : MonoBehaviour
@@ -22,38 +22,20 @@ public class PathBuilder : MonoBehaviour
     }
 
     void UpdateMouseState() {
-        //Debug.Log(MouseState);
-
-        if (Input.GetMouseButton(0)) { // pressed left button
-            if (MouseState == MouseButtons.Nothing) {
-                MouseState = MouseButtons.Left;
-            }
-            else if (MouseState == MouseButtons.Right) {
+        if (Input.GetMouseButton((int)MouseButtons.Left)) {
+            if (Input.GetMouseButton((int)MouseButtons.Right)) {
                 MouseState = MouseButtons.Both;
             }
-        }
-        else { // unpressed left button
-            if (MouseState == MouseButtons.Left) {
-                MouseState = MouseButtons.Nothing;
+            else {
+                MouseState = MouseButtons.Left;
             }
-            else if (MouseState == MouseButtons.Both) {
+        }
+        else {
+            if (Input.GetMouseButton((int)MouseButtons.Right)) {
                 MouseState = MouseButtons.Right;
             }
-        }
-        if (Input.GetMouseButton(1)) { // pressed right button
-            if (MouseState == MouseButtons.Nothing) {
-                MouseState = MouseButtons.Both;
-            }
-            else if (MouseState == MouseButtons.Left) {
-                MouseState = MouseButtons.Both;
-            }
-        }
-        else { // unpressed right button
-            if (MouseState == MouseButtons.Right) {
+            else {
                 MouseState = MouseButtons.Nothing;
-            }
-            else if (MouseState == MouseButtons.Both) {
-                MouseState = MouseButtons.Left;
             }
         }
     }
