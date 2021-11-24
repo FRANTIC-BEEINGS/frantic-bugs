4747e44df52241c32f66ab4a383e372dc9fbc068
ZakharovI <trolev_2012@mail.ru>
2021-11-24 03:40:36 +0300

    the number of different card types has been added

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -22,30 +22,40 @@ public class MapGeneration : MonoBehaviour  {
     // MapId - то, как видит данное поле игрок (содержит в себе id карточек) (должно приходить от сервера)
     // CardPrefabs - список всевозможных карточек
     [SerializeField] private List<GameObject> CardPrefabs;
+    [SerializeField] private List<int> CardTypeCnt;
 
     private List<List<GameObject>> Map;
     private List<List<int>> MapId;
 
 
     void Start() {
-        Initialization();
-        GetMapId();
+        SetMapId();
         InstantiateCards();
     }
 
-    void Initialization() {
-        // получить всякие константы от сервера
-    }
 
-    void GetMapId() {
+    void SetMapId() {
         MapId = new List<List<int>>();
         for (int i = 0; i < MapCardHeight; ++i) {
             MapId.Add(new List<int>());
             for (int j = 0; j < MapCardWidth; ++j) {
-                MapId[i].Add(Random.Range(0, CardPrefabs.Count));
+                MapId[i].Add(-1);
+            }
+        }
+
+        for (int CardId = 0; CardId < CardTypeCnt.Count; ++CardId) {
+            for (int cnt = 0; cnt < CardTypeCnt[CardId]; ++cnt) {
+                int i = Random.Range(0, MapId.Count);
+                int j = Random.Range(0, MapId[i].Count);
+                while (MapId[i][j] != -1) {
+                    i = Random.Range(0, MapId.Count);
+                    j = Random.Range(0, MapId[i].Count);
+                }
+                MapId[i][j] = CardId;
             }
         }
     }
+
 
     void InstantiateCards() {
         Map = new List<List<GameObject>>();
@@ -76,6 +86,7 @@ public class MapGeneration : MonoBehaviour  {
             }
         }
     }
+
 
     void Update() {
 
