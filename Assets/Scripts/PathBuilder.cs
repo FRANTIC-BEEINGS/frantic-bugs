0eeaf8a02299ed965ae4c9afe5946a76b623e209
ZakharovI <trolev_2012@mail.ru>
2021-11-26 01:42:05 +0300

    add OnCard boolean variable for cursor

--- Assets/Scripts/PathBuilder.cs
+++ Assets/Scripts/PathBuilder.cs
@@ -9,16 +9,57 @@ enum MouseButtons {
     Both
 }
 
+enum BuildingState {
+    Forbidden, // wait for Allowed
+    Allowed,   // wait for input
+    Started,   // wait for finished
+    Finished   // go to Forbidden
+}
+
 public class PathBuilder : MonoBehaviour
 {
     [SerializeField] private MouseButtons MouseState;
+    [SerializeField] private BuildingState PathState;
+    [SerializeField] private bool OnCard;
+    public List<GameObject> Path;
 
     void Start() {
         MouseState = MouseButtons.Nothing;
+        PathState = BuildingState.Forbidden;
+        OnCard = false;
     }
 
     void Update() {
+        UpdateOnCard();
         UpdateMouseState();
+        UpdateBuildingState();
+    }
+
+    void UpdateBuildingState() {
+      /*
+        if (PathState == BuildingState.Forbidden) {
+            // Каким-то образом узнать, можно ли строить путь.
+            if (true) {
+                PathState = BuildingState.Allowed;
+            }
+        }
+        else if (PathState == BuildingState.Allowed) {
+            if (MouseState == MouseButtons.Left) {
+                //
+            }
+        }
+      */
+    }
+
+    void UpdateOnCard() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit;
+        OnCard = false;
+        if (Physics.Raycast(ray, out rayHit, 100.0f)) {
+            if (rayHit.collider.tag == "Card") {
+                OnCard = true;
+            }
+        }
     }
 
     void UpdateMouseState() {
