5298ded248c5d77f76122003f99f6f82a84a8087
Ekaterina <kitekate2000cat@gmail.com>
2021-11-27 12:33:43 +0300

    add energy handling

--- Assets/Scripts/ResourceManagment/ResourceManager.cs
+++ Assets/Scripts/ResourceManagment/ResourceManager.cs
@@ -1,19 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using Cards;
+using UnityEngine;
 
 namespace ResourceManagment
 {
     public class ResourceManager
     {
         private Dictionary<ResourceType, int> resources;
+        private int _maxEnergy;
+        private int _extraEnergy;
+        
         public Action<ResourceCard> GlobalReplenish;
-        //todo: handle energy
+        
         public ResourceManager()
         {
             resources = new Dictionary<ResourceType, int>();
         }
+        
+        public ResourceManager(int maxEnergy)
+        {
+            resources = new Dictionary<ResourceType, int>();
+            _maxEnergy = maxEnergy;
+        }
 
+        public int GetEnergy()
+        {
+            return resources.ContainsKey(ResourceType.Energy) ? resources[ResourceType.Energy] : 0;
+        }
+
+        public void ReplenishEnergy()
+        {
+            if (resources.ContainsKey(ResourceType.Energy))
+            {
+                resources[ResourceType.Energy] = _maxEnergy;
+            }
+            else
+            {
+                resources.Add(ResourceType.Energy,_maxEnergy);
+            }
+        }
+        
         public void AddResource(ResourceType resource, int quantity)
         {
             if (resources.ContainsKey(resource))
@@ -24,12 +51,15 @@ namespace ResourceManagment
             {
                 resources.Add(resource,quantity);
             }
+
+            if (resource == ResourceType.Energy)
+                _extraEnergy = resources[resource] > _maxEnergy ? resources[resource] - _maxEnergy : 0;
         }
 
         //adds replenishment func (must be called when captured resource card)
         public void AddReplenishableResource()
         {
-            //todo: write test function
+            //todo: fix this
             GlobalReplenish += Replenish;
         }
 
