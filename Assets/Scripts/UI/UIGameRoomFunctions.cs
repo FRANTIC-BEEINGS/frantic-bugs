7f13bda75051666c156125d7354370552e278667
Ekaterina <kitekate2000cat@gmail.com>
2022-01-15 06:30:07 +0300

    add some game room functionality and UI

---
+++ Assets/Scripts/UI/UIGameRoomFunctions.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class UIGameRoomFunctions : MonoBehaviourPunCallbacks, UIController
+    {
+        private MessageLogUI _messageLog;
+        public void AddMessageLog(MessageLogUI messageLog)
+        {
+            _messageLog = messageLog;
+        }
+
+        public void LeaveRoom()
+        {
+            _messageLog?.AddMessage("Leaving room " + PhotonNetwork.CurrentRoom?.Name);
+            PhotonNetwork.LeaveRoom();
+            if (_messageLog != null)
+                Destroy(_messageLog.gameObject.transform.parent.gameObject);
+            SceneManager.LoadScene("Lobby");
+        }
+    }
+}
